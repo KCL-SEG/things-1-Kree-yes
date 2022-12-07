b0e453455e2f96ef223cbc342f2daa75848c6162
Kree-yes <k21053754@kcl.ac.uk>
2022-12-07 19:32:03 +0000

    Add constraints to Thing attributes

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(max_length=120, blank=True)
+    quantity = models.PositiveIntegerField(max_length=100)
 
